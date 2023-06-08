8a21fa7fe75d801d151208a7ef1898b5289d189c
Gyana Ranjan Barik <gyanabarik794@gmail.com>
2023-06-08 23:30:26 +0530

    final update

---
+++ BackEnd/models/Category.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
